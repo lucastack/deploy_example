6f57b1f4213a5bf5bc73ad0bdbbce7cb1288c538
lucastack <lucs.suarez@gmail.com>
2023-07-08 04:10:34 -0400

    add delay threshold in prepare_dataset function

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -7,7 +7,7 @@ from .utils import flight_delay_minutes, get_day_phase, is_high_season
 def prepare_dataset(data_path: str, delay_threshold: int = 15) -> pd.DataFrame:
     raw_df = pd.read_csv(data_path)
     raw_df["delay_minutes"] = raw_df.apply(flight_delay_minutes, axis=1)
-    raw_df["is_delayed"] = np.where(raw_df["delay_minutes"] > 15, 1, 0)
+    raw_df["is_delayed"] = np.where(raw_df["delay_minutes"] > delay_threshold, 1, 0)
     del raw_df["delay_minutes"]
     raw_df["is_high_season"] = raw_df["Fecha-I"].apply(is_high_season)
     raw_df["day_phase"] = raw_df["Fecha-I"].apply(get_day_phase)
